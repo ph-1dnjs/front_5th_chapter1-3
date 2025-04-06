3285341aac8908a61ad5dfcd74791cd42a8176ff
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-06 17:24:34 +0900

    feat: 비교 함수 구현하기 - shallowEquals 함수는 최상위 레벨만 비교하므로 중첩된 구조는 비교할 수 없다. - deepEquals 함수는 모든 레벨을 재귀적으로 비교하여 깊은 비교가 가능하지만 재귀 호출이 많아 느릴 수 있다.

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,28 @@
+import { isObject } from "./utils";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((value, key) => deepEquals(value, objB[key]));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    return (
+      entriesA.length === entriesB.length &&
+      entriesA.every(([key, value]) => {
+        return deepEquals(value, objB[key]);
+      })
+    );
+  }
+
+  return false;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,28 @@
+import { isObject } from "./utils";
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((value, key) => objB[key] === value);
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const entriesA = Object.entries(objA);
+    const entriesB = Object.entries(objB);
+
+    return (
+      entriesA.length === entriesB.length &&
+      entriesA.every(([key, value]) => {
+        return objB[key] === value;
+      })
+    );
+  }
+
+  return false;
 }
