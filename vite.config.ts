3ecac509291c37e968fdf5a8ff994aae75499202
ph-1dnjs <ph.1dnjs@gmail.com>
2025-04-09 20:55:00 +0900

    feat: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default mergeConfig(
   defineConfig({
+    base: isProd ? "/front_5th_chapter1-2/" : "/",
     plugins: [react()],
   }),
   defineTestConfig({
@@ -16,5 +19,5 @@ export default mergeConfig(
         reporter: ["lcov", "json", "json-summary"],
       },
     },
-  }),
+  })
 );
